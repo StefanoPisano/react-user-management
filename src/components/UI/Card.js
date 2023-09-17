4e2f8c2aa325171d8549204b580f0d905c445db9
Stefano Pisano <s.pisano@techgap.it>
2023-09-17 17:50:07 +0200

    feat: adds Card element

---
+++ src/components/UI/Card.js
@@ -0,0 +1,13 @@
+
+import styles from "./Card.module.css"
+
+function Card(props){
+	const classes = `${props.className} ${styles.card}`
+	return (
+		<div className={classes}>
+			{props.children}
+		</div>
+	)
+}
+
+export default Card
